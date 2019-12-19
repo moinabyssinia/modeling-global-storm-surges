3f4b98dec154a50a073cf04ad83784e33eff5cb5
WahlInstall <WahlInstall@CECSGS1RCP2.net.ucf.edu>
2019-12-19 10:17:55 -0500

    commiting the scrpts that will be used to prepare the data for training

---
+++ read_netcdf_v2.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 10:10:09 2019
+
+@author: Michael Tadesse
+"""
+import os
+import pandas as pd
+from netCDF4 import Dataset
+
+
+#####################
+# Reading NetCDF file
+#####################
+
+def readnetcdf(path, predictor, year):
+    """ 
+    reads components of a netcdf file
+    predictor: 'slp', 'uwnd', 'vwnd'
+    year: start_end
+    """
+    var = {"slp":"msl", "uwnd":"u10", "vwnd":"v10"}
+    
+    os.chdir(path)
+    pred_year = str(predictor)+"_"+str(year)+".nc"
+    #print(pred_year)
+    
+    f = str()
+    for ii in os.listdir():
+        if ii.endswith(pred_year):
+            f = ii
+    #print(f)
+    g = Dataset(os.path.abspath(f))
+    #print(g.variables)
+    
+    lon, lat, time, pred = pd.DataFrame(g.variables['longitude'][:]), \
+        pd.DataFrame(g.variables['latitude'][:]),\
+            pd.DataFrame(g.variables['time'][:]), g.variables[var[predictor]][:]
+  
+    return lon, lat, time, pred
+
+
+
---
+++ starter.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 09:30:37 2019
+
+@author: Michael Tadesse
+"""
+
+import os 
+import pandas as pd
+
+script_path = "C:/Users/WahlInstall/Documents/ml_project_v3/scripts"
+os.chdir(script_path)
+
+data_path = "C:/Users/WahlInstall/Documents/ml_project_v3/data"
+
+from define_grid import Coordinate, findPixels, findindx
+from read_netcdf_v2 import readnetcdf
+from subset import subsetter
+from surgets import add_date
+
+
+#define tide gauge
+tg_cord = Coordinate(8.7167, 53.867)
+
+#read netcdf files
+nc_files = readnetcdf(data_path, 'vwnd', '2003_2006')
+lon, lat, time, pred = nc_files[0], nc_files[1], nc_files[2], nc_files[3]
+
+#subset lon/lat/pred
+close_grids = findPixels(tg_cord, 5, lon, lat)
+ind_grids = findindx(close_grids, lon, lat)
+pred_sub = subsetter(pred, ind_grids, time)
+
+
+#choose surge time series
+surge = pd.read_csv('cuxhaven-germany-bsh.mat.mat.csv', header=None)
+surge_with_date = add_date(surge)
