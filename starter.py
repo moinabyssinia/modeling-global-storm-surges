9c17d89b5b87f06f5a0cbf782ece42cda28d39b2
WahlInstall <WahlInstall@CECSGS1RCP2.net.ucf.edu>
2019-12-19 16:15:43 -0500

    update

--- starter.py
+++ starter.py
@@ -18,36 +18,37 @@ from read_netcdf_v2 import readnetcdf
 from subset import subsetter
 from surgets import add_date
 
-##################
 #define tide gauge
-##################
+
 
 tg_cord = Coordinate(8.7167, 53.867)
 
-##################
+
 #read netcdf files
-##################
+
+os.chdir(data_path)
+files = pd.DataFrame(os.listdir())
 
 nc_files = readnetcdf(data_path, 'vwnd', '2003_2006')
 lon, lat, time, pred = nc_files[0], nc_files[1], nc_files[2], nc_files[3]
 
-####################
+
 #subset lon/lat/pred
-####################
+
 close_grids = findPixels(tg_cord, 5, lon, lat)
 ind_grids = findindx(close_grids, lon, lat)
 pred_sub = subsetter(pred, ind_grids, time)
 
-#########################
+
 #choose surge time series
-#########################
+
 surge = pd.read_csv('cuxhaven-germany-bsh.mat.mat.csv', header=None)
 surge_with_date = add_date(surge)
 
-#####################################################
+
 #this is to be done last
 #merge predictors and predictand with 'date' as a key
-#####################################################
+
 pred_surge = pd.merge(pred_sub, surge_with_date.iloc[:,8:10], \
                       on = "date", how = "inner")
 
