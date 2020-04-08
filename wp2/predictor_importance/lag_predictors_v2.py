9e014fbbb17dadbd8a607fe4e3b8c116ec10e8d7
moinabyssinia <WahlInstall@CECSGS1RCP2.net.ucf.edu>
2020-04-08 11:52:48 -0400

    updated predictor lagging script

--- wp2/predictor_importance/lag_predictors_v2.py
+++ wp2/predictor_importance/lag_predictors_v2.py
@@ -10,98 +10,86 @@ import pandas as pd
 import datetime as dt #used for timedelta
 from datetime import datetime
 
-dir_in = 'F:\eraint_D5'
-dir_out = 'F:\eraint_combined_predictors'
+dir_in = 'F:\\eraint_combined_predictors'
+dir_out = 'F:\\eraint_lagged_predictors'
 
-#load predictors
-#load corresponding surge time series
-os.chdir('F:\\dmax_surge')
-surge = pd.read_csv('abashiri-japan-jma.csv')
+#cd to the predictor directory
 
-os.chdir('F:\\eraint_combined_predictors')
-pred = pd.read_csv('abashiri-japan-jma.csv')
+os.chdir(dir_in)
 
-"""
-check redundancy of surge timeseries
-some tide gauges have more than one daily max surge
-"""
-duplicate = surge.duplicated(subset='ymd', keep='first')
-surge = surge[~duplicate]
+#looping through TGs
+
+for tg in range(len(os.listdir())):
+    print(tg)    
+    tg_name = os.listdir()[tg]
+    print(tg_name, '\n')
+
+    pred = pd.read_csv(tg_name)
 
 
-#in some cases the surge ts might start earlier than pred ts
-#use the join function to filter the overlapping period
-
-surge_time = pd.DataFrame(surge['ymd'])
-surge_time.columns = ['date']
-
-
- 
-
-
-#need to change the string format of dates to datetime
-#these will be the time lagging dataframes
-time_str = lambda x: str(datetime.strptime(x, '%Y-%m-%d'))
-time_stamp = lambda x: datetime.strptime(x, '%Y-%m-%d')
-time_converted_str = pd.DataFrame(map(time_str, surge_time['date']), columns = ['date'])
-time_converted_stamp = pd.DataFrame(map(time_stamp, surge_time['date']), columns = ['timestamp'])
-
-#prepare lagged time series for time only
-time_lagged = pd.DataFrame()
-lag_hrs = [0, 6, 12, 18, 24, 30]
-for lag in lag_hrs:
-    lag_name = 'lag'+str(lag)
-    lam_delta = lambda x: str(x - dt.timedelta(hours = lag))
-    lag_new = pd.DataFrame(map(lam_delta, time_converted_stamp['timestamp']), \
-                           columns = [lag_name])
-    time_lagged = pd.concat([time_lagged, lag_new], axis = 1)
+    #create a daily time series - date_range
+    #get only the ymd of the start and end times
+    start_time = pred['date'][0].split(' ')[0]
+    end_time = pred['date'].iloc[-1].split(' ')[0]
     
-#datafrmae that contains all lagged time series  (just time)
-time_all = pd.concat([time_converted_str, time_lagged], axis = 1)
-
-
-
-#find overlapping period for surge and predictors
-pred_subset = pd.merge(pred, time_all['date'], on = ['date'], how = 'right')
-pred_subset.sort_values(by = 'date', inplace=True)
-
-
-#remove rows that have nans
-is_nan = pred_subset.isnull()
-row_has_nan = is_nan.any(axis = 1)
-pred_subset_wo_nans = pred_subset[~row_has_nan]
-
-"""
-#but how can I subset pred based on the surge time
-#for instance, how can I use the lambda function to
-#select pred values that match '1979-01-02 00:00:00'
-#witout using a for loop
-"""
-
-
-"""
-first prepare the six time lagging dataframes  
-then use the merge function to merge the original 
-predictor with the lagging dataframes
-"""
-
-pred_lagged = pd.DataFrame()
-for ii in range(1,time_all.shape[1]): #to loop through the lagged time series
-    print(ii)
-    #extracting corresponding tag time series
-    lag_ts = pd.DataFrame(time_all.iloc[:,ii]) 
-    lag_ts.columns = ['date']
-    #merge the selected tlagged time with the predictor on = "date"
-    pred_new = pd.merge(pred_subset_wo_nans, lag_ts, on = ['date'], how = 'left')
-    pred_new.drop('Unnamed: 0', axis = 1, inplace = True)
-    #sometimes nan values go to the bottom of the dataframe
-    pred_new.sort_values(by = 'date', inplace=True)
+    print(start_time, ' - ', end_time, '\n')
     
-    #concatenate lagged dataframe
-    if ii == 1:
-        pred_lagged = pred_new
-    else: 
-        pred_lagged = pd.merge(pred_lagged, pred_new, on = ['date'], how = 'left')
+    date_range = pd.date_range(start_time, end_time, freq = 'D')
+    
+    
+    #defining time changing lambda functions
+    time_str = lambda x: str(x)
+    time_converted_str = pd.DataFrame(map(time_str, date_range), columns = ['date'])
+    time_converted_stamp = pd.DataFrame(date_range, columns = ['timestamp'])
+    
+    
+    #prepare lagged time series for time only
+    time_lagged = pd.DataFrame()
+    lag_hrs = [0, 6, 12, 18, 24, 30]
+    for lag in lag_hrs:
+        lag_name = 'lag'+str(lag)
+        lam_delta = lambda x: str(x - dt.timedelta(hours = lag))
+        lag_new = pd.DataFrame(map(lam_delta, time_converted_stamp['timestamp']), \
+                               columns = [lag_name])
+        time_lagged = pd.concat([time_lagged, lag_new], axis = 1)
+        
+    #datafrmae that contains all lagged time series  (just time)
+    time_all = pd.concat([time_converted_str, time_lagged], axis = 1)
+    
+    
+    """
+    first prepare the six time lagging dataframes  
+    then use the merge function to merge the original 
+    predictor with the lagging dataframes
+    """
+    
+    pred_lagged = pd.DataFrame()
+    for ii in range(1,time_all.shape[1]): #to loop through the lagged time series
+        print(ii, '\n')
+        #extracting corresponding tag time series
+        lag_ts = pd.DataFrame(time_all.iloc[:,ii]) 
+        lag_ts.columns = ['date']
+        #merge the selected tlagged time with the predictor on = "date"
+        pred_new = pd.merge(pred, lag_ts, on = ['date'], how = 'right')
+        pred_new.drop('Unnamed: 0', axis = 1, inplace = True)
+        #sometimes nan values go to the bottom of the dataframe
+        #sort df by date -> reset the index -> remove old index
+        pred_new.sort_values(by = 'date', inplace=True)
+        pred_new.reset_index(inplace=True)
+        pred_new.drop('index', axis = 1, inplace= True)
+        
+        #concatenate lagged dataframe
+        if ii == 1:
+            pred_lagged = pred_new
+        else: 
+            pred_lagged = pd.concat([pred_lagged, pred_new.iloc[:,1:]], axis = 1)
+            
+    
+    #cd to saving directory
+    os.chdir(dir_out)
+    pred_lagged.to_csv(tg_name)
+    os.chdir(dir_in)
+
     
     
 
