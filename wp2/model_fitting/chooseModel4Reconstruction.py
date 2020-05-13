74f34e6f5c0d4c36276db1ad558211786bc09a14
moinabyssinia <michaelg.tadesse@gmail.com>
2020-05-13 09:55:26 -0400

    adding model selector

--- wp2/model_fitting/chooseModel4Reconstruction.py
+++ wp2/model_fitting/chooseModel4Reconstruction.py
@@ -16,10 +16,12 @@ def chooseModel():
     #import packages
     import os
     import pandas as pd
+    import shutil
     
+    #define directories
     dir_in = 'F:\\06_eraint_results\\model_fitting'
-    dir_out_surge = 'F:\\08_eraint_surge_reconstruction\\bestReconstruction\\surgeReconstruced'
-    dir_out_metadata = 'F:\\08_eraint_surge_reconstruction\\bestReconstruction\\metaData'
+    dir_out_surge = 'F:\\08_eraint_surge_reconstruction\\bestReconstruction\\surgeReconstructed'
+   #dir_out_metadata = 'F:\\08_eraint_surge_reconstruction\\bestReconstruction\\metaData'
     
     #read kfold validation csvs
     os.chdir(dir_in)
@@ -32,6 +34,57 @@ def chooseModel():
        'corrn_x', 'rmse_x', 'corrn_y', 'rmse_y']]
     comp.columns = ['tg', 'lon', 'lat', 'num_year', 'num_95pcs',
        'corrn_lr', 'rmse_lr', 'corrn_rf', 'rmse_rf']
+    comp['bestModel'] = 'nan'
     
-    #filter comp based on RMSE results
-    comp[comp['rmse_lr'] <= comp['rmse_rf']]
+    #filter comparison (lr/rf) based on RMSE results(as it says more about
+    #the model accuracy than correlation)
+    for ii in comp[comp['rmse_lr'] <= comp['rmse_rf']].index:
+        comp['bestModel'][ii] = 'MLR'
+    for jj in comp[comp['rmse_lr'] > comp['rmse_rf']].index:
+        comp['bestModel'][jj] = 'RF'
+    for kk in comp[comp['rmse_lr'].isna()].index:
+        comp['bestModel'][kk] = 'RF'
+    for rr in comp[comp['rmse_rf'].isna()].index:
+        comp['bestModel'][rr] = 'MLR'
+        
+    #copy files to bestReconstruction based on RMSE value
+    for ii in range(len(comp)):
+        if comp['bestModel'][ii] == 'MLR':
+            print(comp['bestModel'][ii])
+            os.chdir('F:\\08_eraint_surge_reconstruction\\lreg')
+            source = os.path.join(os.path.abspath(os.getcwd()), comp['tg'][ii])
+            destination = os.path.join(dir_out_surge, comp['tg'][ii])
+            
+            try:
+                shutil.copyfile(source, destination)
+                #print("file copied successfully!")
+            except shutil.SameFileError:
+                print("source and destination represent the same file")
+            except:
+                print("error occured while copying file")
+        else:
+            comp['bestModel'][ii]
+            os.chdir('F:\\08_eraint_surge_reconstruction\\randomForest')
+            source = os.path.join(os.path.abspath(os.getcwd()), comp['tg'][ii])
+            destination = os.path.join(dir_out_surge, comp['tg'][ii])
+            
+            try:
+                shutil.copyfile(source, destination)
+                #print("file copied successfully!")
+            except shutil.SameFileError:
+                print("source and destination represent the same file")
+            except:
+                print("error occured while copying file")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
