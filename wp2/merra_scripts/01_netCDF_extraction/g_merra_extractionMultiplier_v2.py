72d9a5d02b5f49a35a337e99852ee695045f391c
moinabyssinia <michaelg.tadesse@gmail.com>
2020-06-22 10:44:17 -0400

    updated merra combining script

--- wp2/merra_scripts/01_netCDF_extraction/g_merra_extractionMultiplier_v2.py
+++ wp2/merra_scripts/01_netCDF_extraction/g_merra_extractionMultiplier_v2.py
@@ -8,16 +8,16 @@ MERRA multiple scripts producer
 """
 import os
 
-os.chdir('D:\\data\\scripts\\modeling_storm_surge\\wp2\\merra_scripts\\01_netCDF_extraction\\merra902TG')
+os.chdir('D:\\data\\scripts\\modeling_storm_surge\\wp2\\merra_scripts\\01_netCDF_extraction\\merra902Combine')
 
 
-for ii in range(0, 903):
+for ii in range(0, 902):
     print(ii)
 
     # save_name_nc = '_'.join(['era', '20c', var_name[var_list[ii]], \
     #                       str(years[jj]), '.nc'])
     save_name_py = ''.join([str(ii), '-tideGauge', '.py'])
-    f = open('b_merra_extract_data_original_v3.py', 'r')
+    f = open('b_merra_combined_predictors_v3STOKES.py', 'r')
     filedata = f.read()
     f.close()
     
