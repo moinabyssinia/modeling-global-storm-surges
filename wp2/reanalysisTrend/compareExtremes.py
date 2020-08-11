160b2d0043b7424c9069c38e7656f4442a411d35
moinabyssinia <michaelg.tadesse@gmail.com>
2020-08-11 12:10:19 -0400

    added loader and extreme getter

--- wp2/reanalysisTrend/compareExtremes.py
+++ wp2/reanalysisTrend/compareExtremes.py
@@ -2,10 +2,33 @@
 """
 Created on Tue Aug 11 11:43:56 2020
 
-
+compare the extremes in observed and reconstructed
+surges for a longer period of time
 
 @author: Michael Tadesse
 """
+import os 
+import pandas as pd
 
 
-vic1909 = vic20cr[vic20cr['year'] == 1909]
+def getExtremes(timeSeries, percentile):
+    """
+    this function gets the extremes in the 
+    observation and their corresponding
+    reconstructed values
+    """
+    
+    
+
+def loadTimesSeries(tideGauge, reanalysis):
+    """
+    this function loads a time series
+    for the chosen tide gauge
+    
+    tideGauge = ['victoria', 'fremantle', 'brest', 'atlanticCity']
+    reanalysis = ['Twcr', 'Era20c']
+    """
+    os.chdir("G:\\data\\reanalysisTrendFiles\\reconSurgeFiles")
+    dat = pd.read_csv(tideGauge+reanalysis+"Merged.csv")
+    dat.drop(['Unnamed: 0', 'index', 'Unnamed: 0.1'], axis = 1, inplace = True)
+    return dat
