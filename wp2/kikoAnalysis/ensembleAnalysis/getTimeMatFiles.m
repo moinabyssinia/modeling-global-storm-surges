cf20fb5fd29f95df880aecd405995bd8dfd0cddf
moinabyssinia <michaelg.tadesse@gmail.com>
2021-02-22 17:28:58 -0500

    change matlab datenum

---
+++ wp2/kikoAnalysis/ensembleAnalysis/getTimeMatFiles.m
@@ -0,0 +1,14 @@
+%to change datenum matlab time to normal time %
+
+%load files 
+dir_in = "G:\\05_era5\\kikoStuff\\ensembleData";
+chdir(dir_in);
+
+ensList = dir('*.mat');
+
+for ii = 1:100
+  ensList(ii).name
+  
+  %load file 
+endfor  
+  
