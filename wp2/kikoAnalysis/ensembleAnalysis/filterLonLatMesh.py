0943dcec3b3487fab1d97de15cb3c5844c9523cc
moinabyssinia <michaelg.tadesse@gmail.com>
2021-02-25 09:44:20 -0500

    rearranged functions

--- wp2/kikoAnalysis/ensembleAnalysis/filterLonLatMesh.py
+++ wp2/kikoAnalysis/ensembleAnalysis/filterLonLatMesh.py
@@ -22,43 +22,6 @@ os.chdir(dir_in)
 ensList = os.listdir()
 
 
-#change matlab date to python date
-def datenum_to_datetime(datenum):
-    """
-    Convert Matlab datenum into Python datetime.
-    :param datenum: Date in datenum format
-    :return:        Datetime object corresponding to datenum.
-    """
-    days = datenum % 1
-    return datetime.fromordinal(int(datenum)) \
-           + timedelta(days=days) \
-           - timedelta(days=366)
-           
-def getMesh(lon, lat, tgLon, tgLat):
-    """
-    get a 2x2 mesh lonlat
-    """
-    x = (lat >= tgLat-2) & (lat <= tgLat+2);
-    y = (lon >= tgLon-2) & (lon <= tgLon+2);
-    xy = (x == True) & (x == y);
-    # (xy[xy == True]).count(axis = 1) #count True values
-    return xy;
-
-def subsetPredictor(mesh, slp, wsp):
-    """
-    subset predictor in 2x2 mesh
-    
-    * get lat - get lon - subset based on tg
-    * get intersection of lon lat subset
-    * simply subset predictor with intersection
-    
-    """
-    newSLP = pd.DataFrame(slp[mesh]).T;
-    newWSP = pd.DataFrame(wsp[mesh]).T;
-    
-    return newSLP, newWSP;
-
-
 def eXtract():
     """
     extract slp and wsp from 100 ensemble
@@ -77,13 +40,13 @@ def eXtract():
     
         #get time    
         time = pd.DataFrame(matFile['time'])
-        getTime = lambda x: datenum_to_datetime(x)
+        getTime = lambda x: datenum_to_datetime(x).strftime('%Y-%m-%d')
         time = pd.DataFrame(list(map(getTime, time[0])))
+        time.columns = ['date'];
         
-        time['date'] = 'nan'
-        for ii in range(len(time)):
-            time['date'][ii] = \
-                datenum_to_datetime(time[0][ii]).strftime('%Y-%m-%d')
+        # time['date'] = 'nan'
+        # for ii in range(len(time)):
+        #     time['date'][ii] = datenum_to_datetime(time[0][ii])
     
         #predictors
         slp = matFile['slp']
@@ -144,6 +107,45 @@ def eXtract():
                 
                 tgSLP.to_csv(tgName+"SLP.csv")
                 tgWSP.to_csv(tgName+"WSP.csv")
+
+
+#change matlab date to python date
+def datenum_to_datetime(datenum):
+    """
+    Convert Matlab datenum into Python datetime.
+    :param datenum: Date in datenum format
+    :return:        Datetime object corresponding to datenum.
+    """
+    days = datenum % 1
+    return datetime.fromordinal(int(datenum)) \
+           + timedelta(days=days) \
+           - timedelta(days=366)
+           
+def getMesh(lon, lat, tgLon, tgLat):
+    """
+    get a 2x2 mesh lonlat
+    """
+    x = (lat >= tgLat-2) & (lat <= tgLat+2);
+    y = (lon >= tgLon-2) & (lon <= tgLon+2);
+    xy = (x == True) & (x == y);
+    # (xy[xy == True]).count(axis = 1) #count True values
+    return xy;
+
+def subsetPredictor(mesh, slp, wsp):
+    """
+    subset predictor in 2x2 mesh
+    
+    * get lat - get lon - subset based on tg
+    * get intersection of lon lat subset
+    * simply subset predictor with intersection
+    
+    """
+    newSLP = pd.DataFrame(slp[mesh]).T;
+    newWSP = pd.DataFrame(wsp[mesh]).T;
+    
+    return newSLP, newWSP;
+
+
         
         
 
